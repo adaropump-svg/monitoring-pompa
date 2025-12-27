d87abf73211edbafda4d9c87e852e1c4b928d154
adaropump-svg <adaropump@gmail.com>
2025-12-27 14:11:42 +0800

    initial upload monitoring-pompa app

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("pump-monitor-v1").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "dashboard.html",
+        "menu-ops.html",
+        "menu-mon.html",
+        "menu-hce.html",
+        "data-record.html",
+        "grafik.html",
+        "setting.html",
+        "assets/app-config.js",
+        "assets/app-theme.css"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
